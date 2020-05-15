f2ca9027ad9e74f6c7fd5e43c398624e8292ea4c
KingCosmic <KingCosmicDev@gmail.com>
2020-05-15 18:07:07 -0500

    added a basic sprite class and removed unused code

--- src/classes/Sprite.ts
+++ src/classes/Sprite.ts
@@ -1,36 +1,23 @@
 import Phaser from 'phaser';
 import * as Planck from 'planck-js';
 
-class Box extends Phaser.GameObjects.Sprite {
+class Sprite extends Phaser.GameObjects.Sprite {
   isDynamic: boolean
   isFixed: boolean
 
   body: Planck.Body
 
-  constructor(scene: Phaser.Scene, x: number, y: number, width: number, height: number, isDynamic: boolean, isFixed: boolean) {
-    super(scene, x, y, '')
+  constructor(scene: Phaser.Scene, x: number, y: number, texture: string, isDynamic: boolean, isFixed: boolean) {
+    super(scene, x, y, texture)
 
-    const rnd = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-    const graphics = scene.add.graphics()
-    graphics.fillStyle(0xEEEEEE, 1)
-    graphics.fillRect(0, 0, width, height)
-    graphics.generateTexture(rnd, width, height)
-    graphics.destroy()
-
-    this.setTexture(rnd)
     this.isDynamic = isDynamic
     this.isFixed = isFixed
     this.scene.add.existing(this)
 
-    this.width = width
-    this.height = height
-    this.x = x + width / 2
-    this.y = y - height / 2
-
     this.body = scene.planck.world.createBody()
     if (this.isDynamic) this.body.setDynamic()
 
-    this.body.createFixture(Planck.Box(width / 2 / scene.planck.scaleFactor, height / 2 / scene.planck.scaleFactor))
+    this.body.createFixture(Planck.Box(this.width / 2 / scene.planck.scaleFactor, this.height / 2 / scene.planck.scaleFactor))
     this.body.setPosition(Planck.Vec2(this.x / scene.planck.scaleFactor, this.y / scene.planck.scaleFactor))
     this.body.setMassData({
       mass: 1,
@@ -49,4 +36,4 @@ class Box extends Phaser.GameObjects.Sprite {
   }
 }
 
-export default Box
+export default Sprite
